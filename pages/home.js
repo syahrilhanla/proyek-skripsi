5ec547297a82b57d343611ef746f2e615ddf6dd8
syahrilhanla <35387698+syahrilhanla@users.noreply.github.com>
2021-03-11 18:25:30 +0800

    banner added

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import HomeNavbar from '../components/HomeNavbar';
+import homeStyle from '../styles/HomePage.module.css';
 
 const home = () => {
   return (
     <div>
       <HomeNavbar />
-      This is a landing page
+      <section className={homeStyle.billBoard}>
+        <h1>Media Pembelajaran Statistika</h1>
+        <h1>Kelas VIII</h1>
+        <div className={homeStyle.startButton}>
+          <span>Start</span>
+        </div>
+      </section>
     </div>
   )
 }
