80f927a78165a7a6936073fd22e3e9beb86da805
syahrilhanla <35387698+syahrilhanla@users.noreply.github.com>
2021-03-11 23:28:13 +0800

    tambahan bg-image

--- pages/home.js
+++ pages/home.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import HomeNavbar from '../components/HomeNavbar';
 import homeStyle from '../styles/HomePage.module.css';
+import Link from 'next/link';
 
 const home = () => {
   return (
     <div>
       <HomeNavbar />
       <section className={homeStyle.billBoard}>
+        <div class={homeStyle.bgImage}></div>
         <h1>Media Pembelajaran Statistika</h1>
         <h1>Kelas VIII</h1>
         <div className={homeStyle.startButton}>
-          <span>Start</span>
+          <Link href={'/'}>Mulai</Link>
         </div>
       </section>
     </div>
