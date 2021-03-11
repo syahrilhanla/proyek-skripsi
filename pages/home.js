2c8deb2bed96fa5ecc782ce64b1d1cf2a0b3d168
syahrilhanla <35387698+syahrilhanla@users.noreply.github.com>
2021-03-11 23:36:25 +0800

    responsive sectionA diperbaiki

--- pages/home.js
+++ pages/home.js
@@ -7,15 +7,25 @@ const home = () => {
   return (
     <div>
       <HomeNavbar />
-      <section className={homeStyle.billBoard}>
-        <div class={homeStyle.bgImage}></div>
-        <h1>Media Pembelajaran Statistika</h1>
-        <h1>Kelas VIII</h1>
-        <div className={homeStyle.startButton}>
-          <Link href={'/'}>Mulai</Link>
-        </div>
+      <SectionA />
+      <section className={homeStyle.sectionB}>
+        <h1>Mengapa App Bagus Untuk Kamu:</h1>
       </section>
     </div>
+  )
+}
+
+
+function SectionA() {
+  return (
+    <section className={homeStyle.billBoard}>
+      <div class={homeStyle.bgImage}></div>
+      <h1>Media Pembelajaran Statistika</h1>
+      <h1>Kelas VIII</h1>
+      <div className={homeStyle.startButton}>
+        <Link href={'/'}>Mulai</Link>
+      </div>
+    </section>
   )
 }
 
