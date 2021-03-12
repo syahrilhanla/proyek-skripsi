329a49f48c2259496a993b76d26abbe4bb416bed
syahrilhanla <35387698+syahrilhanla@users.noreply.github.com>
2021-03-12 09:37:10 +0800

    reasons done maybe?

--- pages/home.js
+++ pages/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomeNavbar from '../components/HomeNavbar';
 import homeStyle from '../styles/HomePage.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const home = () => {
   return (
@@ -14,14 +15,23 @@ const home = () => {
           <div class={homeStyle.reasons}>
             <ul>
               <li>
+                <span>
+                  <Image src={'/open-book.png'} width={70} height={70} className={homeStyle.images} />
+                </span>
                 <h3>Belajar Mandiri</h3>
                 <p>Belajar dengan kecepatanmu sendiri</p>
               </li>
               <li>
+                <span>
+                  <Image src={'/quiz.png'} width={70} height={70} />
+                </span>
                 <h3>Latihan</h3>
                 <p>Kamu akan diberikan latihan untuk menguji dan mengasah pemahamanmu</p>
               </li>
               <li>
+                <span>
+                  <Image src={'/progress.png'} width={70} height={70} />
+                </span>
                 <h3>Progress Tracking</h3>
                 <p>Proses belajarmu akan dipantau sehingga kamu mengetahui seberapa pesat perkembanganmu</p>
               </li>
