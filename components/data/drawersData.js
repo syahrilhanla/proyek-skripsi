081a3487171c2f429c8e88a793c82fa1bfde3d9f
syahrilhanla <35387698+syahrilhanla@users.noreply.github.com>
2021-03-20 15:41:14 +0800

    Drawer already works

---
+++ components/data/drawersData.js
@@ -0,0 +1,75 @@
+export const data = [
+  {
+    id: 1,
+    title: 'Menganalisis Data',
+    items: [
+      {
+        id: 'Menganalisis Tabel',
+        subTitle: 'Menganalisis Tabel'
+      },
+      {
+        id: 'Menganalisis Grafik',
+        subTitle: 'Menganalisis Grafik'
+      },
+      {
+        id: 'Kuis 1',
+        subTitle: 'Kuis 1'
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: 'Ukuran Pemusatan Data',
+    items: [
+      {
+        id: 'Mean',
+        subTitle: 'Mean'
+      },
+      {
+        id: 'Median',
+        subTitle: 'Median'
+      },
+      {
+        id: 'Modus',
+        subTitle: 'Modus'
+      },
+      {
+        id: 'Kuis 2',
+        subTitle: 'Kuis 2'
+      },
+    ]
+  },
+  {
+    id: 3,
+    title: 'Ukuran Penyebaran Data',
+    items: [
+      {
+        id: 'Jangkauan Data',
+        subTitle: 'Jangkauan Data'
+      },
+      {
+        id: 'Kuartil',
+        subTitle: 'Kuartil'
+      },
+      {
+        id: 'Jangkauan Antarkuartil',
+        subTitle: 'Jangkauan Antarkuartil'
+      },
+      {
+        id: 'Simpangan Kuartil',
+        subTitle: 'Simpangan Kuartil'
+      },
+      {
+        id: 'Kuis 3',
+        subTitle: 'Kuis 3'
+      },
+    ]
+  }
+];
+
+export const commonLinks = [
+  {
+    text: 'About',
+    link: '/about'
+  }
+]
