e9a7d2cfd1403934eb0f17112cd8f57cb9748d41
syahrilhanla <35387698+syahrilhanla@users.noreply.github.com>
2021-03-30 20:15:53 +0800

    # Refactored lines to some functions # Added some explainatory comments

--- components/utils/userFirestoreSavings.js
+++ components/utils/userFirestoreSavings.js
@@ -1,26 +1,56 @@
 import { firestore } from '../Firebase';
 
-// documents references
+// ROOT DOCUMENTS REFERENCE
 const docRef = firestore.collection('users');
 
-const createInitProgress = (collection, initialData) => {
-  collection.doc('page1').set(initialData);
-}
 
+// used in createUserProgress
+// UID from localStorage credential, initialData currently from createUserProgress below
 const createDocRefs = (uid, initialData) => {
-  try {
-    const chapter1 = docRef.doc(uid).collection('chapter1');
-    const chapter2 = docRef.doc(uid).collection('chapter2');
-    const chapter3 = docRef.doc(uid).collection('chapter3');
 
-    createInitProgress(chapter1, initialData);
-    createInitProgress(chapter2, initialData);
-    createInitProgress(chapter3, initialData);
+  // function to create document references
+  const createInitProgress = (collection, initialData) => {
+    docRef.doc(uid).collection(collection).doc('page1').set(initialData);
+  }
+
+  try {
+    createInitProgress('chapter1', initialData);
+    createInitProgress('chapter2', initialData);
+    createInitProgress('chapter3', initialData);
   } catch (error) {
     console.log(`failed to create init refs: ${error}`);
   }
 }
 
+// fired when there is no user checked in the initial loading
+export const createUserProgress = async (localUser) => {
+
+  if (localUser) {
+    // this data is only for experiment for now 
+    const initialData = { act: true };
+
+    console.log('progress created');
+
+    createDocRefs(localUser.uid, initialData);
+
+  } else return;
+}
+
+// THIS FUNCTION HAS NOT BEEN USED YET
+export const updateProgress = async (localUser, newData) => {
+  if (localUser) {
+    // data is just for experiment for now 
+    console.log('updating progress');
+
+    // docs id below only used for testing
+    const data = docRef.doc(localUser.uid).collection('progress');
+    await data.doc('upylsZjAbdW7b6O7lkx2').update({
+      "initialData.quiz1": 8
+    });
+    console.log('progress updated');
+
+  } else return;
+}
 
 export const getUserFirestore = async (localUser) => {
   if (localUser) {
@@ -38,23 +68,30 @@ export const getUserFirestore = async (localUser) => {
 export const getUserProgress = async (localUser) => {
 
   if (localUser) {
-    const chapter1Page1 = await docRef.doc(localUser.uid).collection('chapter1').doc('page1').get();
 
-    // chapter1Page2 and so on...
-    // const chapter1Page2 = await docRef.doc(localUser.uid).collection('chapter1').doc('doc1').collection('page2').get();
+    // getting chapters data through function with String as its argument
+    const getChapterData = async (collection) => {
+      const chapterData = await (docRef.doc(localUser.uid)
+        .collection(collection)
+        .get());
 
-    const chapter2Page1 = await docRef.doc(localUser.uid).collection('chapter2').doc('page1').get();
+      // returning the docs of the collection as an array
+      return chapterData.docs.map(doc => doc.data());
+    }
 
-    const chapter3Page1 = await docRef.doc(localUser.uid).collection('chapter2').doc('page1').get();
+    // setting them to variables
+    const chapter1Data = await getChapterData('chapter1');
+    const chapter2Data = await getChapterData('chapter2');
+    const chapter3Data = await getChapterData('chapter3');
 
-    if (!chapter1Page1 || chapter1Page1.length === 0) {
+    // if no progress then returns null, so it creates new progress  
+    if (!chapter1Data || chapter1Data.length === 0) {
       console.log('progress yet not exists');
       return null
     }
 
-    console.log(chapter1Page1.data());
-    console.log('there is a progress');
-    const results = chapter1Page1.data();
+    // returning results as objects within an array
+    const results = [{ chapter1: chapter1Data }, { chapter2: chapter2Data }, { chapter3: chapter3Data }];
 
     console.log(results);
 
@@ -63,6 +100,7 @@ export const getUserProgress = async (localUser) => {
 }
 
 export const addUser = async (localUser) => {
+  // creating user if there's none, with the credentials from localStorage 
   if (localUser) {
     const userData = {
       displayName: localUser.displayName,
@@ -70,37 +108,10 @@ export const addUser = async (localUser) => {
       email: localUser.email
     }
 
+    // setting them to firestore so it can be used in data display
     const addData = docRef.doc(localUser.uid);
     await addData.set(userData)
     console.log('user added');
 
   } else return;
-}
-
-export const createUserProgress = async (localUser) => {
-
-  if (localUser) {
-    // data is just for experiment for now 
-    const initialData = { act: true }
-
-    console.log('progress created');
-
-    createDocRefs(localUser.uid, initialData);
-
-  } else return;
-}
-
-export const updateProgress = async (localUser, newData) => {
-  if (localUser) {
-    // data is just for experiment for now 
-    console.log('updating progress');
-
-    const data = docRef.doc(localUser.uid).collection('progress');
-    await data.doc('upylsZjAbdW7b6O7lkx2').update({
-      "initialData.quiz1": 8
-    });
-    console.log('progress updated');
-
-  } else return;
-
 }
