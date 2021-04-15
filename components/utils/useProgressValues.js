25571878ab55153572ff9bbb3c7490c9c0003453
syahrilhanla <35387698+syahrilhanla@users.noreply.github.com>
2021-04-15 09:11:35 +0800

    deleted unused files

--- components/utils/useProgressValues.js
+++ components/utils/useProgressValues.js
@@ -8,18 +8,20 @@ const useProgressValues = () => {
 
   const { localUserData } = useAuth();
 
-  const [chapter1Value, setChapter1Value] = useState(0);
-  const [chapter2Value, setChapter2Value] = useState(0);
-  const [chapter3Value, setChapter3Value] = useState(0);
+  const initData = { score: 0, actLength: 0, percentage: 0 };
+
+  const [chapter1Value, setChapter1Value] = useState(initData);
+  const [chapter2Value, setChapter2Value] = useState(initData);
+  const [chapter3Value, setChapter3Value] = useState(initData);
 
   const setProgressValues = () => {
     return { chapter1Value, chapter2Value, chapter3Value, }
   }
 
-  const setUserProgress = () => {
-    const chapter1Progress = combinePageProgress(getLocalUserProgress('chapter1'));
-    const chapter2Progress = combinePageProgress(getLocalUserProgress('chapter2'));
-    const chapter3Progress = combinePageProgress(getLocalUserProgress('chapter3'));
+  const setUserProgress = async () => {
+    const chapter1Progress = combinePageProgress(await getLocalUserProgress('chapter1'));
+    const chapter2Progress = combinePageProgress(await getLocalUserProgress('chapter2'));
+    const chapter3Progress = combinePageProgress(await getLocalUserProgress('chapter3'));
 
     setChapter1Value(getScore(chapter1Progress.combinedProgress));
     setChapter2Value(getScore(chapter2Progress.combinedProgress));
