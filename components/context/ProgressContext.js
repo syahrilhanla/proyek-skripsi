0632cc0273b9501eac279e8dd4bc94701d0dab9f
syahrilhanla <syahrilhanla5@gmail.com>
2021-04-17 09:38:00 +0800

    merged conflicts

--- components/context/ProgressContext.js
+++ components/context/ProgressContext.js
@@ -1,13 +1,8 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useAuth } from "@/components/context/AuthContext";
 import {
-<<<<<<< HEAD
 	dataSeparator,
 	getCurrentPageProgress,
-=======
-  dataSeparator,
-  getCurrentPageProgress,
->>>>>>> 36b6b4b608bdea223f175814247441ce22cfae2c
 } from "@/components/utils/dataProcessors";
 import { useRouter } from "next/router";
 
