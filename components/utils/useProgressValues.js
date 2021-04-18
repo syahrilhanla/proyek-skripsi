163336db8431f5b601d15b69e6b6d840582d431c
syahrilhanla <syahrilhanla5@gmail.com>
2021-04-18 10:12:19 +0800

    refactored dashboard to use costum hooks

--- components/utils/useProgressValues.js
+++ components/utils/useProgressValues.js
@@ -1,21 +1,19 @@
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
 
 import {
 	combinePageProgress,
 	getScore,
 } from "@/components/utils/dataProcessors";
-import { getLocalUserProgress } from "@/components/utils/userLocalSavings";
-=======
-import { combinePageProgress, getScore } from "@/components/utils/dataProcessors";
-import { getLocalUserProgress } from '@/components/utils/userLocalSavings';
->>>>>>> 36b6b4b608bdea223f175814247441ce22cfae2c
 
 import { useAuth } from "@/components/context/AuthContext";
+import { useProgress } from "@/components/context/ProgressContext";
 
 const useProgressValues = () => {
-<<<<<<< HEAD
 	const { localUserData } = useAuth();
+	const { dashboardProgress, dashboardLoading } = useProgress();
+
+	const [pageReady, setPageReady] = useState(false);
+
 	const [chapter1Value, setChapter1Value] = useState(0);
 	const [chapter2Value, setChapter2Value] = useState(0);
 	const [chapter3Value, setChapter3Value] = useState(0);
@@ -24,57 +22,38 @@ const useProgressValues = () => {
 		return { chapter1Value, chapter2Value, chapter3Value };
 	};
 
-	const setUserProgress = () => {
-		getLocalUserProgress("chapter1").then((data) => {
-			setChapter1Value(getScore(combinePageProgress(data).combinedProgress));
-		});
+	const setUserProgress = async (dashboardProgress) => {
+		console.log(dashboardProgress);
+		if (dashboardProgress.chapter1 !== undefined) {
+			const chapter1Progress = combinePageProgress(
+				await dashboardProgress.chapter1
+			);
+			const chapter2Progress = combinePageProgress(
+				await dashboardProgress.chapter2
+			);
+			const chapter3Progress = combinePageProgress(
+				await dashboardProgress.chapter3
+			);
 
-		getLocalUserProgress("chapter2").then((data) => {
-			setChapter2Value(getScore(combinePageProgress(data).combinedProgress));
-		});
+			setChapter1Value(getScore(chapter1Progress.combinedProgress));
+			setChapter2Value(getScore(chapter2Progress.combinedProgress));
+			setChapter3Value(getScore(chapter3Progress.combinedProgress));
 
-		getLocalUserProgress("chapter3").then((data) => {
-			setChapter3Value(getScore(combinePageProgress(data).combinedProgress));
-		});
+			setPageReady(true);
+		}
 	};
 
 	useEffect(() => {
-		setUserProgress();
-		return () => console.log("oke");
-	}, []);
+		if (dashboardProgress !== null) {
+			console.log(dashboardProgress);
+			setUserProgress(dashboardProgress);
+		}
+	}, [dashboardLoading]);
 
-	return { localUserData, setProgressValues };
+	return {
+		localUserData,
+		setProgressValues,
+		pageReady,
+	};
 };
-
 export default useProgressValues;
-=======
-
-  const { localUserData } = useAuth();
-
-  const [chapter1Value, setChapter1Value] = useState(0);
-  const [chapter2Value, setChapter2Value] = useState(0);
-  const [chapter3Value, setChapter3Value] = useState(0);
-
-  const setProgressValues = () => {
-    return { chapter1Value, chapter2Value, chapter3Value, }
-  }
-
-  const setUserProgress = () => {
-    const chapter1Progress = combinePageProgress(getLocalUserProgress('chapter1'));
-    const chapter2Progress = combinePageProgress(getLocalUserProgress('chapter2'));
-    const chapter3Progress = combinePageProgress(getLocalUserProgress('chapter3'));
-
-    setChapter1Value(getScore(chapter1Progress.combinedProgress));
-    setChapter2Value(getScore(chapter2Progress.combinedProgress));
-    setChapter3Value(getScore(chapter3Progress.combinedProgress));
-  }
-
-  useEffect(() => {
-    setUserProgress();
-  }, []);
-
-  return { localUserData, setProgressValues }
-}
-
-export default useProgressValues;
->>>>>>> 36b6b4b608bdea223f175814247441ce22cfae2c
