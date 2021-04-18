0dcc5f5a2af66e0cd94b5b7a4a7bf1c89bdfd5b2
syahrilhanla <syahrilhanla5@gmail.com>
2021-04-18 11:10:14 +0800

    refactor dashboard progress to custom hooks

--- components/context/ProgressContext.js
+++ components/context/ProgressContext.js
@@ -1,13 +1,9 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { useAuth } from "@/components/context/AuthContext";
-import { dataSeparator } from "@/components/utils/dataProcessors";
+import { createContext, useContext } from "react";
+import { useRouter } from "next/router";
 
-import {
-	setLocalProgress,
-	getLocalUserProgress,
-} from "@/components/utils/userLocalSavings";
-
-import useGetCurrentChapterProgress from "@/components/utils/useGetCurrentChapterProgress";
+import useSetDashboardProgress from "../utils/useSetDashboardProgress";
+import { getCurrentPageProgress } from "../utils/dataProcessors";
+import { getLocalUserProgress } from "../utils/userLocalSavings";
 
 const ProgressContext = createContext();
 
@@ -16,84 +12,39 @@ export const useProgress = () => {
 };
 
 const ProgressProvider = ({ children }) => {
-	// userProgress is a promise, so use .then() and .catch()
-	// can't use async await because this root function is used to render globally
-	// so it should not be a promise
-	const { userProgress } = useAuth();
+	const setProgress = useSetDashboardProgress();
 
-	const [dashboardLoading, setDashboardLoading] = useState(true);
-	const [dashboardProgress, setDashboardProgress] = useState(null);
-
-	/*
-    EXPECTED OUTPUT of userProgress:
-    [
-      { chapter: chapter(n),
-        data: [
-          {
-            page1: [act1: Boolean, ... act(n): Boolean]
-          },  
-          {
-            page(n): [act1: Boolean, ... act(n): Boolean]
-          }
-        ]
-      }
-    ]
-  */
-
-	const getProgress = async (userProgress) => {
-		// this also checked in when logging out, and the data will be null
-
-		const data = await userProgress;
-
-		// if no data or logged out, then exit function immediately;
-		if (!data) return;
-
-		// EXPECTED OUTPUT OF dataSeparator
-		// [
-		// 	{
-		//   	page(n): [act1: Boolean, ... act(n): Boolean]
-		// 	}
-		// ]
-
-		const chapter1 = dataSeparator(data, "chapter1");
-		const chapter2 = dataSeparator(data, "chapter2");
-		const chapter3 = dataSeparator(data, "chapter3");
-
-		await setLocalProgress("chapter1", chapter1);
-		await setLocalProgress("chapter2", chapter2);
-		await setLocalProgress("chapter3", chapter3);
-	};
-
-	const settingUpProgress = async () => {
-		await getProgress(userProgress);
-		const chapter1 = await getLocalUserProgress("chapter1");
-		const chapter2 = await getLocalUserProgress("chapter2");
-		const chapter3 = await getLocalUserProgress("chapter3");
-
-		console.log(chapter1);
-		setDashboardProgress({ chapter1, chapter2, chapter3 });
-	};
-
-	// getting the value of the promise, then separate them to each array,
-	// setting them to localState
-	useEffect(() => {
-		settingUpProgress();
-	}, [userProgress]);
-
-	useEffect(() => {
-		if (dashboardProgress !== null) {
-			setDashboardLoading((prevState) => !prevState);
-			console.log(dashboardLoading);
+	// used to get the wanted chapter progress
+	const router = useRouter();
+	const getCurrentChapterProgress = async () => {
+		const currentChapter = router.pathname.split("/")[1];
+		const currentPage = router.pathname.split("/")[2];
+		console.log({ currentChapter }, { currentPage });
+		if (currentChapter === "analisis") {
+			return getCurrentPageProgress(
+				await getLocalUserProgress("chapter1"),
+				`page${currentPage}`
+			);
+		} else if (currentChapter === "pemusatan") {
+			return getCurrentPageProgress(
+				await getLocalUserProgress("chapter2"),
+				`page${currentPage}`
+			);
+		} else if (currentChapter === "penyebaran") {
+			return getCurrentPageProgress(
+				await getLocalUserProgress("chapter3"),
+				`page${currentPage}`
+			);
 		}
-	}, [dashboardProgress]);
+	};
 
 	return (
 		<ProgressContext.Provider
 			value={{
 				useProgress,
-				useGetCurrentChapterProgress,
-				dashboardLoading,
-				dashboardProgress,
+				getCurrentChapterProgress,
+				dashboardLoading: setProgress.dashboardLoading,
+				dashboardProgress: setProgress.dashboardProgress,
 			}}
 		>
 			{children}
