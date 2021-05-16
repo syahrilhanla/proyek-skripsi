12777dbb5d6d31ffac1f76a1ad766792c30bdebb
syahrilhanla <syahrilhanla5@gmail.com>
2021-05-16 19:39:17 +0800

    added texts on about page

--- pages/about.js
+++ pages/about.js
@@ -30,7 +30,7 @@ function AboutContent() {
 				<p style={{ marginBottom: "0.6rem", marginTop: "-1.5rem" }}>
 					<span style={{ fontSize: "21px" }}>M</span>ateri disusun dengan
 					pendekatan kontekstual sehingga peserta didik dapat memahami apa
-					hubungan dan manfaat dari statistika dalam kehidupan sehari-hari
+					hubungan dan manfaat dari statistika dalam kehidupan sehari-hari.
 				</p>
 
 				<h1>Teknologi yang Digunakan</h1>
@@ -54,7 +54,7 @@ function AboutContent() {
 				<h1>Profil Pengembang</h1>
 				<p>
 					Syahril Hanla Azis (1710131110017), mahasiswa Pendidikan Komputer,
-					Fakultas Keguruan dan Ilmu Pendidikan, Universitas Lambung Mangkurat
+					Fakultas Keguruan dan Ilmu Pendidikan, Universitas Lambung Mangkurat.
 				</p>
 
 				<h1>Kepentingan Pengembangan</h1>
@@ -65,15 +65,20 @@ function AboutContent() {
 						Pengembangan Media Pembelajaran Interaktif Berbasis Web pada Materi
 						Statistika Kelas VIII dengan Pendekatan Kontekstual Lahan Basah
 					</i>
-					"
+					. "
 				</p>
 
 				<h1>Ucapan Terima Kasih</h1>
 				<p>
 					Terima kasih diucapkan sebesar-besarnya kepada para Dosen Pembimbing,
 					yakni Bapak Dr. Harja Santanapurba, M. Kom. selaku{" "}
-					<i>Dosen Pembimbing 1</i>, dan Bapak Novan A. B. Saputra, S. Kom., M.
-					T. selaku <i>Dosen Pembimbing 2</i>.
+					<i>Dosen Pembimbing 1</i>, dan Ibu Yuni Suryaningsih, M. Pd. selaku{" "}
+					<i>Dosen Pembimbing 2</i>.
+				</p>
+				<p>
+					Tidak lupa juga kepada teman-teman seperjuangan dan tim dosen dan staf
+					program studi yang selama ini telah bekerja sama, membantu, dan
+					mendidik sehingga tugas akhir ini dapat terselesaikan.
 				</p>
 			</div>
 		</div>
