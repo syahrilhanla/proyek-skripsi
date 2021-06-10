c1c509eea51a8a8b3ecd1dd9bceaea4397e134a2
syahrilhanla <syahrilhanla5@gmail.com>
2021-06-10 17:07:18 +0800

    checkActivity with no problem, but conflicted with ssr

--- components/utils/useCheckActivity.js
+++ components/utils/useCheckActivity.js
@@ -1,18 +1,24 @@
-import { useMemo } from "react";
+import { useEffect } from "react";
+import dynamic from "next/dynamic";
+import createActivityDetector from "activity-detector";
 
 // pass in hooks from MainLayout component to update its value
-const useCheckActivity = (isActive, setIsActive) => {
 
-	// fires every time user moves the mouse or pressing keys to reset the count down back to 5 minutes
-	const resetTimer = () => {
-		setTimeout(() => setIsActive(false), 300000);
-	};
+const useCheckActivity = dynamic(
+	(setIsActive, option) => {
+		// fires every time user moves the mouse or pressing keys to reset the count down back to 5 minutes
 
-	// only reset the timer if and only if isActive's value changes (after the modal popped up and closed)
-	const activeMemo = useMemo(() => resetTimer, [isActive]);
-
-	// reset count if user interacts with the app
-	document.addEventListener("mousemove", activeMemo);
-	document.addEventListener("keypress", activeMemo);
-};
+		useEffect(() => {
+			console.log(typeof window);
+			if (typeof (window !== "undefined")) {
+				console.log("window is object");
+				const activityDetector = createActivityDetector(option);
+				activityDetector.on("idle", () => setIsActive(false));
+				// activityDetector.on("active", () => setUserActive());
+				return () => activityDetector.stop();
+			}
+		}, []);
+	},
+	{ ssr: false }
+);
 export default useCheckActivity;
