59da6848a6726744fba57a75c4beac7b59e45ef1
syahrilhanla <syahrilhanla5@gmail.com>
2021-07-06 08:48:05 +0800

    tambah error message di HookForm

--- components/common/HookForm.js
+++ components/common/HookForm.js
@@ -2,7 +2,11 @@ import { useForm } from "react-hook-form";
 import formStyle from "@/styles/Form.module.css";
 
 const HookForm = () => {
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 	const onSubmit = (data) => console.log(data);
 
 	return (
@@ -12,6 +16,9 @@ const HookForm = () => {
 					{...register("className", { required: true })}
 					className={formStyle.formInput}
 				/>
+				{errors.className?.type === "required" && (
+					<p className={formStyle.errorMsg}>Isi Nama Kelas!</p>
+				)}
 				<input type='submit' className={formStyle.submitButton} />
 			</form>
 		</div>
