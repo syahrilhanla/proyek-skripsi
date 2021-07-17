e3bf7a2ded0758c117d618d9e8be07b87313db53
syahrilhanla <syahrilhanla5@gmail.com>
2021-07-17 08:58:54 +0800

    display each user's overall progress on admin page

--- components/common/UserCard.js
+++ components/common/UserCard.js
@@ -1,14 +1,25 @@
 import { Avatar } from "@material-ui/core";
 import { getUserProgress } from "@/components/utils/userFirestoreSavings";
 
+import userCardStyles from "@/styles/UserCard.module.css";
+import BorderLinearProgress from "@/components/common/BorderLinearProgress";
+
 const UserCard = ({ userData }) => {
 	return (
-		<div>
-			<span onClick={() => console.log(getUserProgress(userData))}>
-				<Avatar alt={userData.displayName} src={userData.photoURL} />
-				<p key={userData.displayName}>{userData.displayName}</p>
-			</span>
-		</div>
+		<>
+			<div
+				className={userCardStyles.parentDiv}
+				onClick={() => console.log(getUserProgress(userData))}
+			>
+				<span className={userCardStyles.avatar}>
+					<Avatar alt={userData.displayName} src={userData.photoURL} />
+				</span>
+				<h3 key={userData.displayName}>{userData.displayName}</h3>
+				<span className={userCardStyles.userProgress}>
+					<BorderLinearProgress value={80} />
+				</span>
+			</div>
+		</>
 	);
 };
 export default UserCard;
