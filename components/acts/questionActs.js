908c1136713691eed47c8375a55578dc17e4216c
syahrilhanla <syahrilhanla5@gmail.com>
2021-08-18 16:43:10 +0800

    made data seperated from component

---
+++ components/acts/questionActs.js
@@ -0,0 +1,16 @@
+export const questionDataAnalysisPage4 = [
+	{
+		id: "act2",
+		questionText: "question data analysis page 4?",
+		answerChoices: [
+			{
+				answerText: "this is it",
+				isCorrect: true,
+			},
+			{
+				answerText: "this is not it",
+				isCorrect: false,
+			},
+		],
+	},
+];
---
+++ components/acts/scrollActs.js
@@ -0,0 +1,37 @@
+export const analysisPage4 = {
+	// should be act1, now its just for testing
+	id: "act1",
+	act: (
+		<div>
+			<p>
+				Data biasanya juga disajikan dalam bentuk grafik atau diagram, dengan
+				menggunakan tampilan ini biasanya data yang lebih kompleks dapat lebih
+				mudah untuk dipahami tanpa perlu usaha lebih untuk menganalisisnya.
+				Sebagai contoh kita mempunyai sebuah tabel dengan data ukuran ikan
+				Kelabau yang tertangkap di sungai. Kita bisa mengubahnya dari tabel
+				menjadi berbagai macam grafik atau diagram, misalnya diagram batang,
+				diagram lingkaran, dan diagram garis.
+			</p>
+			<p>
+				Di samping merupakan data dari hasil kumpulan tangkapan ikan Kelabau
+				yang ada di sepanjang sungai Kapuas. Berdasarkan hasil tangkapan
+				tersebut kita dapat menyajikan datanya menjadi sebuah tabel yang
+				menggolongkan ikan berdasarkan ukurannya, kemudian per golongan ukuran
+				tersebut didapatkan persentase dari jumlah keseluruhan. Berdasarkan data
+				yang ini kita dapat membuatnya menjadi berbagai macam jenis diagram. Di
+				bawah merupakan hasil dari pembuatan diagram batang menggunakan data
+				yang sama pada tabel di atas.
+			</p>
+			<p>
+				Di samping merupakan data dari hasil kumpulan tangkapan ikan Kelabau
+				yang ada di sepanjang sungai Kapuas. Berdasarkan hasil tangkapan
+				tersebut kita dapat menyajikan datanya menjadi sebuah tabel yang
+				menggolongkan ikan berdasarkan ukurannya, kemudian per golongan ukuran
+				tersebut didapatkan persentase dari jumlah keseluruhan. Berdasarkan data
+				yang ini kita dapat membuatnya menjadi berbagai macam jenis diagram. Di
+				bawah merupakan hasil dari pembuatan diagram batang menggunakan data
+				yang sama pada tabel di atas.
+			</p>
+		</div>
+	),
+};
