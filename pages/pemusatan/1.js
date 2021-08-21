fcc523d5de9ada2c64b85b71923c9463547016fc
syahrilhanla <syahrilhanla5@gmail.com>
2021-08-21 09:40:43 +0800

    adjusted files for pemusatan and penyebaran chpter

--- pages/pemusatan/1.js
+++ pages/pemusatan/1.js
@@ -1,42 +1,17 @@
-import React from "react";
 import MainLayout from "@/components/common/MainLayout";
-import DisplayEquation from "@/components/charts/KatexComponent";
-import LearningObjectives from "@/components/common/LearningObjectives";
-import { chapter2Objectives } from "@/components/data/objectivesData";
+import { pemusatanPage1 } from "@/components/acts/scrollActs";
+import TableOne from "@/components/graphsAndTables/TableOne";
 
 const page1 = () => {
-	const Text = () => (
-		<div
-			style={{
-				padding: "0.8rem",
-				display: "flex",
-				justifyContent: "center",
-				flexWrap: "wrap",
-			}}
-		>
-			<LearningObjectives objectives={chapter2Objectives} />
-			<p>
-				Kamu mungkin pernah mendengar dengan kata “rata-rata”, “paling
-				tinggi/rendah”, atau “paling berat/ringan”. Contohnya ada pada kalimat
-				seperti “rata-rata tinggi orang Indonesia adalah 158,12 cm”, atau
-				“gunung tertinggi di dunia adalah Gunung Everest”. Kalimat-kalimat
-				tersebut didapatkan dari data yang diperoleh sebelumnya melalui
-				pengumpulan dan pengolahan data. Inilah yang disebut dengan statistika,
-				yaitu ilmu yang mempelajari tentang pengumpulan, pengolahan,
-				penganalisisan, dan penarikan kesimpulan data.
-			</p>
-			<p>
-				Di sini kamu akan mempelajari apa itu statistika berdasarkan masalah
-				yang ditemui di kehidupan sehari-hari melalui data berbentuk tabel dan
-				diagram. Selain itu kamu akan mempelajari cara menentukan nilai
-				rata-rata (mean), median, modus, dan sebaran data.
-			</p>
+	const Text = () => pemusatanPage1.act;
 
-			<DisplayEquation>\int_0^\infty x^2 dx</DisplayEquation>
-		</div>
+	return (
+		<MainLayout
+			Child1={TableOne}
+			Child2={Text}
+			title={"Ukuran Pemusatan Data"}
+		/>
 	);
-
-	return <MainLayout Child1={Text} title={"Ukuran Pemusatan Data"} />;
 };
 
 export default page1;
--- pages/pemusatan/index.js
+++ pages/pemusatan/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 const analisisData = () => {
-  const router = useRouter();
-  router.push('/analisis/1');
-  return <></>
-}
+	const router = useRouter();
+	router.push("/pemusatan/1");
+	return <></>;
+};
 
 export default analisisData;
