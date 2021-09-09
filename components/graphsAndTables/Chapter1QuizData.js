6e9fdb83041e096e5bb8821ae53d817e8d770887
syahrilhanla <syahrilhanla5@gmail.com>
2021-09-09 15:05:25 +0800

    fully added pemusatan chapter visual data

--- components/graphsAndTables/Chapter1QuizData.js
+++ components/graphsAndTables/Chapter1QuizData.js
@@ -1,4 +1,7 @@
 import tableStyle from "@/styles/TableStyle.module.css";
+import BarChart from "@/components/charts/BarChart";
+import LineChart from "@/components/charts/LineChart";
+import PieChart from "@/components/charts/PieChart";
 
 export const RiverTable = () => {
 	const riverData = [
@@ -92,13 +95,60 @@ export const FishTable = () => {
 				<tbody>
 					{fishData.map((item, index) => (
 						<tr key={item.length}>
-							<td>{index}</td>
+							<td>{index + 1}</td>
 							<td>{item.length}</td>
 							<td>{item.amount}</td>
 						</tr>
 					))}
 				</tbody>
 			</table>
+		</div>
+	);
+};
+
+export const RiverCatch = () => {
+	const labels = ["Baung", "Paray", "Udang Galah", "Lais"];
+	const values = [325, 275, 325, 375];
+
+	const chartData = {
+		labels: labels,
+		label: "Hasil Tangkapan Sungai",
+		data: values,
+	};
+
+	return <BarChart chartData={chartData} />;
+};
+
+export const SnakeHeadLine = () => {
+	const chartData = {
+		labels: ["Bulan 1", "Bulan 2", "Bulan 3", "Bulan 4", "Bulan 5"],
+		datasets: [
+			{
+				label: "Ikan Gabus",
+				data: [3, 6, 9, 12, 23],
+				fill: false,
+				backgroundColor: "#ff9340",
+				borderColor: "#ff9340",
+			},
+		],
+	};
+
+	return (
+		<div>
+			<LineChart chartData={chartData} />
+		</div>
+	);
+};
+
+export const DuckPie = () => {
+	const chartData = {
+		labels: ["Itik Sehat", "Itik Sakit"],
+		data: [18, 82],
+	};
+
+	return (
+		<div style={{ margin: "auto" }}>
+			<PieChart chartData={chartData} />
 		</div>
 	);
 };
--- pages/analisis/kuis.js
+++ pages/analisis/kuis.js
@@ -3,8 +3,11 @@ import QuizComponent from "@/components/common/QuizComponent";
 import { questionData } from "@/components/data/quiz1Data";
 import Caption from "@/components/common/Caption";
 import {
+	DuckPie,
 	FishTable,
+	RiverCatch,
 	RiverTable,
+	SnakeHeadLine,
 } from "@/components/graphsAndTables/Chapter1QuizData";
 
 const quiz1 = () => {
@@ -26,6 +29,52 @@ const quiz1 = () => {
 							Data berikut digunakan untuk pertanyaan nomor 2 dan 3
 						</Caption>
 						<FishTable />
+					</>
+				);
+			} else if (currentQuestion < 5) {
+				return (
+					<>
+						<Caption>
+							Data berikut digunakan untuk pertanyaan nomor 4 dan 5
+						</Caption>
+						<p>
+							Diagram berikut merupakan data dari hasil tangkapan nelayan di
+							sungai wilayah Banjarmasin dan sekitarnya yang bersumber dari
+							BPSKB (Badan Pusat Statistik Kota Banjarmasin)
+						</p>
+						<RiverCatch />
+					</>
+				);
+			} else if (currentQuestion < 8) {
+				return (
+					<>
+						<Caption>
+							Data pada tabel berikut digunakan untuk menjawab soal nomor 6
+							sampai 8
+						</Caption>
+						<p>
+							Di bawah merupakan diagram garis untuk menampilkan pertumbuhan
+							dari ukuran Ikan Gabus (cm) per 30 hari. Amati dan analisis data
+							tersebut untuk menjawab pertanyaan yang diberikan.
+						</p>
+						<SnakeHeadLine />
+					</>
+				);
+			} else if (currentQuestion < 10) {
+				return (
+					<>
+						<Caption>
+							Data pada tabel berikut digunakan untuk menjawab soal nomor 9 dan
+							10
+						</Caption>
+						<p>
+							Seorang petani di lahan gambut memiliki sebuah peternakan itik
+							Alabio. Peternakan tersebut terdiri dari 100 ekor itik dengan
+							pendapatan pasti 100 butir telur per harinya. Namun sebagian itik
+							milik petani tersebut terkena penyakit dan sekarang gagal
+							bertelur. Data ini ditampilkan pada diagram lingkaran di bawah.
+						</p>
+						<DuckPie />
 					</>
 				);
 			} else return null;
