8ca7384b749e4c89a691be7b2fb00578d934a0f6
syahrilhanla <syahrilhanla5@gmail.com>
2021-09-16 14:31:17 +0800

    debugged dashboard new user className

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -94,6 +94,7 @@ function UserProgress({ progressValues }) {
 }
 
 function DashboardContent({ displayInfo, acts, setNewClass, userClass }) {
+	console.log(userClass);
 	return (
 		<section className={dashboardStyles.dashboard}>
 			<div className={dashboardStyles.profile}>
@@ -110,8 +111,9 @@ function DashboardContent({ displayInfo, acts, setNewClass, userClass }) {
 					<div className={dashboardStyles.infoContainer}>
 						<div className={dashboardStyles.profileName}>
 							<h1>{displayInfo.displayName}</h1>
-							{displayInfo.className === "Belum Masuk Kelas" &&
-							userClass === "Belum Masuk Kelas" ? (
+							{(displayInfo.className === "Belum Masuk Kelas" &&
+								userClass === "Belum Masuk Kelas") ||
+							displayInfo.className === undefined ? (
 								<p
 									onClick={() => setNewClass(true)}
 									style={{ cursor: "pointer" }}
