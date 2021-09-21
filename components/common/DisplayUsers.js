50b1e9f62792e79d3c2f1f4fd666aa5ba529e3a1
syahrilhanla <syahrilhanla5@gmail.com>
2021-09-21 13:05:19 +0800

    added switch to diplay individual and group detail

--- components/common/DisplayUsers.js
+++ components/common/DisplayUsers.js
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import UserCard from "@/components/common/UserCard";
+import { Switch } from "@material-ui/core";
 
 const DisplayUsers = ({ selectedClass, userList }) => {
+	// if true then display individual progress
+	const [checked, setChecked] = useState(true);
+
+	const handleChange = () => {
+		setChecked(!checked);
+	};
+
 	return (
 		<div>
 			{selectedClass !== "Pilih Kelas" && (
 				<h2 style={{ fontWeight: 400 }}>Kelas {selectedClass}</h2>
 			)}
-			{userList.map((userData) => (
-				<UserCard userData={userData} key={userData.uid} />
-			))}
+
+			{userList.length > 0 ? (
+				<>
+					<div>
+						<span>Tabel Grup</span>
+						<Switch
+							defaultChecked
+							size='medium'
+							color='primary'
+							onChange={handleChange}
+							checked={checked}
+						/>
+						<span>Individu</span>
+					</div>
+
+					{checked &&
+						userList.map((userData) => (
+							<UserCard userData={userData} key={userData.uid} />
+						))}
+				</>
+			) : (
+				<h3 style={{ fontWeight: 400, textAlign: "center" }}>
+					Tidak Ada Siswa di Kelas Ini
+				</h3>
+			)}
 		</div>
 	);
 };
