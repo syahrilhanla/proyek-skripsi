a69ea731ed0dc3bb9a8fe926b1492be15d37b28e
syahrilhanla <syahrilhanla5@gmail.com>
2021-09-22 15:05:42 +0800

    can display users data, and partially on page detail

--- components/common/GroupTable.js
+++ components/common/GroupTable.js
@@ -2,65 +2,70 @@ import { useEffect, useState } from "react";
 import { getUsersDetails } from "@/components/utils/userFirestoreSavings";
 
 const GroupTable = ({ userList }) => {
-	const [allDetails, setAllDetails] = useState([]);
+	const [usersData, setUsersData] = useState([]);
+
 	useEffect(() => {
-		getUsersDetails(userList);
+		getUsersDetails(userList).then((data) => setUsersData(data));
 	}, []);
 
-	useEffect(() => console.log(allDetails), [allDetails]);
-
 	console.table(userList);
+
+	// TODO in this file:
+	// # make group detail table to display
+	// # iterate table data driven by data input
 	return (
-		<div>
-			<table style={{ border: "black 1px solid" }}>
+		<div
+			style={{
+				padding: "1rem",
+				overflowX: "auto",
+				width: "90%",
+				border: "black 1px solid",
+				textAlign: "center",
+			}}
+		>
+			<table>
 				<thead>
 					<tr>
-						<td rowspan='3'>No</td>
-						<td rowspan='3'>Nama</td>
-						<td rowspan='3'>Skor</td>
-						<td colspan='25'>Bab 1: Analisis Data</td>
+						<td rowSpan={3}>No</td>
+						<td rowSpan={3}>Nama</td>
+						<td rowSpan={3}>Skor</td>
+						<td colSpan={25}>Bab 1: Analisis Data</td>
 					</tr>
+					{/* <tr>
+						<td colSpan='5'>Halaman 1</td>
+						<td colSpan='5'>Halaman 2</td>
+						<td colSpan='5'>Halaman 3</td>
+						<td colSpan='5'>Halaman 4</td>
+						<td colSpan='5'>Halaman 5</td>
+					</tr> */}
+					<tr></tr>
 					<tr>
-						<td colspan='5'>Halaman 1</td>
-						<td colspan='5'>Halaman 2</td>
-						<td colspan='5'>Halaman 3</td>
-						<td colspan='5'>Halaman 4</td>
-						<td colspan='5'>Halaman 5</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>2</td>
-						<td>3</td>
-						<td>4</td>
-						<td>5</td>
-						<td>1</td>
-						<td>2</td>
-						<td>3</td>
-						<td>4</td>
-						<td>5</td>
-						<td>1</td>
-						<td>2</td>
-						<td>3</td>
-						<td>4</td>
-						<td>5</td>
-						<td>1</td>
-						<td>2</td>
-						<td>3</td>
-						<td>4</td>
-						<td>5</td>
-						<td>1</td>
-						<td>2</td>
-						<td>3</td>
-						<td>4</td>
-						<td>5</td>
+						{console.table(usersData)}
+						{usersData.length > 0 &&
+							usersData[0]
+								// .filter((chapter) => chapter.chapter === "chapter1")
+								.map((chapterData) =>
+									chapterData.data.map((data, index) => {
+										console.log(data);
+										return (
+											<td
+												colSpan={chapterData.length}
+												style={{ padding: "0.7rem" }}
+											>
+												Hal {index + 1}
+											</td>
+										);
+									})
+								)}
 					</tr>
 				</thead>
+
 				<tbody>
 					{userList.map((user, index) => {
 						return (
 							<tr>
 								<td>{index + 1}</td>
-								<td>{user.displayName}</td>
+								<td style={{ minWidth: "6rem" }}>{user.displayName}</td>
 								<td>{user.score}</td>
 							</tr>
 						);
