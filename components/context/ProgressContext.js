ecb915c3e5e7b3ec0e216e46339388344140855d
syahrilhanla <syahrilhanla5@gmail.com>
2021-10-15 15:20:23 +0800

    can get snapshot data on evaluation key

--- components/context/ProgressContext.js
+++ components/context/ProgressContext.js
@@ -1,8 +1,9 @@
 // THIS CODE IS CRUCIAL FOR ANY PROGRESS IN THE WHOLE APP
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 import useSetProgress from "@/components/utils/useSetProgress";
+import { firestore } from "@/components/common/Firebase";
 
 const ProgressContext = createContext();
 
@@ -13,6 +14,16 @@ export const useProgress = () => {
 const ProgressProvider = ({ children }) => {
 	const setProgress = useSetProgress();
 	const [quizScore, setQuizScore] = useState(0);
+	const [isEvaluationOpen, setIsEvaluationOpen] = useState(true);
+
+	useEffect(() => {
+		const snapShotData = firestore
+			.collection("classNames")
+			.doc("IX C")
+			.onSnapshot((doc) => setIsEvaluationOpen(doc.data().isEvaluationOpen));
+
+		return () => snapShotData();
+	}, [isEvaluationOpen]);
 
 	return (
 		<ProgressContext.Provider
