4974cfa78400a262a225900db5c1e6aae2133d31
syahrilhanla <syahrilhanla5@gmail.com>
2021-11-27 08:45:45 +0800

    open dialog when pressing submit button on evaluation page

--- components/common/SubmitButton.js
+++ components/common/SubmitButton.js
@@ -1,6 +1,9 @@
+import { useState, useEffect } from "react";
+
 import useSubmitAnswers from "@/components/utils/useSubmitAnswers";
 
 import layoutStyles from "@/styles/MainLayout.module.css";
+import AlertDialog from "@/components/common/Dialog";
 
 const SubmitButton = ({
 	questionAmount,
@@ -9,21 +12,42 @@ const SubmitButton = ({
 	overallAnswers,
 	localUserData,
 }) => {
+	const [isDialogOpened, setIsDialogOpened] = useState(false);
+	const [isAnswerSubmitted, setIsAnswerSubmitted] = useState(false);
+
+	useEffect(() => {
+		if (isAnswerSubmitted) {
+			useSubmitAnswers(
+				questionAmount,
+				quizScore,
+				overallAnswers,
+				localUserData
+			);
+			setIsFinished(true);
+		}
+	}, [isAnswerSubmitted]);
+
+	const checkIsFullyAnswered = () => {
+		if (overallAnswers.length === questionAmount) {
+			console.log(overallAnswers.length, questionAmount);
+			return true;
+		} else return false;
+	};
 	return (
-		<button
-			onClick={() => {
-				useSubmitAnswers(
-					questionAmount,
-					quizScore,
-					overallAnswers,
-					localUserData
-				);
-				setIsFinished(true);
-			}}
-			className={layoutStyles.answerButton}
-		>
-			Kumpul Jawaban
-		</button>
+		<>
+			<AlertDialog
+				isFullyAnswered={checkIsFullyAnswered()}
+				isDialogOpened={isDialogOpened}
+				setIsDialogOpened={setIsDialogOpened}
+				setIsAnswerSubmitted={setIsAnswerSubmitted}
+			/>
+			<button
+				onClick={() => setIsDialogOpened(true)}
+				className={layoutStyles.answerButton}
+			>
+				Kumpul Jawaban
+			</button>
+		</>
 	);
 };
 
