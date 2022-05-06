d2dd830bd663310ff70bef4cfd389fd5bb83b990
syahrilhanla <syahrilhanla5@gmail.com>
2022-05-06 10:32:21 +0800

    swapped piechart to diagram image in analisis/5

--- pages/analisis/5.js
+++ pages/analisis/5.js
@@ -1,7 +1,7 @@
 import MainLayout from "@/components/common/MainLayout";
 import { chapter1Page5 } from "@/components/data/shortEssayData";
 import { analysisPage5 } from "@/components/acts/scrollActs";
-import Chapter1Pie from "@/components/graphsAndTables/Chapter1Pie";
+import Image from "next/image";
 
 const page3 = () => {
 	const Text = () => analysisPage5.act;
@@ -9,7 +9,16 @@ const page3 = () => {
 	return (
 		<>
 			<MainLayout
-				Child1={Chapter1Pie}
+				Child1={() => (
+					<div style={{ display: "flex", justifyContent: "center" }}>
+						<Image
+							src={"/PieChart.png"}
+							width={324}
+							height={324}
+							quality={100}
+						/>
+					</div>
+				)}
 				Child2={Text}
 				title={"Ayo Mencoba!"}
 				essayQuestion={chapter1Page5}
