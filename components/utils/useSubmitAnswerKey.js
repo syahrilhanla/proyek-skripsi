fd9a1542b330fb02636fe0abe52fc2be9a8f2f3b
syahrilhanla <syahrilhanla5@gmail.com>
2022-05-21 17:33:48 +0800

    answer key to each quiz chapter has been added

--- components/utils/useSubmitAnswerKey.js
+++ components/utils/useSubmitAnswerKey.js
@@ -1,5 +1,12 @@
+// USED ONLY TO SET ANSWER KEY DATA IN THE DATABASE
+
+// imported data
 import evaluationQuiz from "@/components/data/evaluationQuiz";
-import { submitAnswerKey } from "./userFirestoreSavings";
+import { questionData as quiz1Data } from "@/components/data/quiz1Data";
+import { questionData as quiz2Data } from "@/components/data/quiz2Data";
+import { questionData as quiz3Data } from "@/components/data/quiz3Data";
+
+import { submitAnswerKey } from "@/components/utils/userFirestoreSavings";
 
 const useSubmitAnswerKey = () => {
 	const indexDeterminer = (answer, index) => {
@@ -11,20 +18,26 @@ const useSubmitAnswerKey = () => {
 		}
 	};
 
-	const answerList = [];
-	evaluationQuiz.forEach((data, questionIndex) =>
-		data.answerChoices.forEach((answer, index) => {
-			if (answer.isCorrect)
-				answerList.push({
-					trueAnswer: indexDeterminer(answer.isCorrect, index),
-					number: questionIndex,
-				});
-		})
-	);
+	const makeAnswerObjects = (importedData) => {
+		const answerList = [];
+		importedData.forEach((data, questionIndex) =>
+			data.answerChoices.forEach((answer, index) => {
+				if (answer.isCorrect)
+					answerList.push({
+						trueAnswer: indexDeterminer(answer.isCorrect, index),
+						number: questionIndex,
+					});
+			})
+		);
+		return answerList;
+	};
 
 	// document.addEventListener("dblclick", () => console.log({ answerList }));
 
-	submitAnswerKey(answerList);
+	submitAnswerKey(makeAnswerObjects(evaluationQuiz), "answerList");
+	submitAnswerKey(makeAnswerObjects(quiz1Data), "chapter1");
+	submitAnswerKey(makeAnswerObjects(quiz2Data), "chapter2");
+	submitAnswerKey(makeAnswerObjects(quiz3Data), "chapter3");
 };
 
 export default useSubmitAnswerKey;
